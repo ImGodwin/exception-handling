3b913174d5e89006b8822f7428d37a9d68888d7f
Godwin <godwinmail6@gmail.com>
2023-10-09 15:59:07 +0200

    Car class created, unchecked class with method imported into class, Getters and Setters updated

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
        // #2.
 
-        int[] arr = {3, 5, 7, 9, 4, 6, 8, 1, 2, 0};
+        /*int[] arr = {3, 5, 7, 9, 4, 6, 8, 1, 2, 0};
 
         Scanner input = new Scanner(System.in);
 
@@ -22,6 +22,8 @@ public class Main {
                 //System.out.println(arrPositionUpdate);
                 if(arrPositionUpdate != 0) {
                     System.out.println("enter num again");
+                }else{
+                    System.out.println("Correct num");
                 }
 
 
@@ -29,7 +31,29 @@ public class Main {
                 System.out.println("confirming");
             }finally{
                 input.close();
-            }
+            }*/
+
+        //#3
+
+        //int km, litri;
+
+        Scanner input = new Scanner(System.in);
+
+        try{
+            System.out.println("enter km: ");
+            int carKilometre = Integer.parseInt(input.nextLine());
+            km = carKilometre;
+
+            System.out.println("How many litres of petrol?: ");
+            int petrolConsumed = Integer.parseInt(input.nextLine());
+            litri = petrolConsumed;
+
+
+        }catch (){
+
+        }
+
+
         }
 
 
