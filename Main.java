e831b8b6eec0aeb74ca9fbc97905fcc974270198
Godwin <godwinmail6@gmail.com>
2023-10-09 15:24:44 +0200

    question 2 resolved

--- Main.java
+++ Main.java
@@ -8,21 +8,21 @@ public class Main {
         int[] arr = {3, 5, 7, 9, 4, 6, 8, 1, 2, 0};
 
         Scanner input = new Scanner(System.in);
-        System.out.println("enter num: ");
-        int arrPositionUpdate = Integer.parseInt(input.nextLine());
-        do {
+
 
             try{
-
+                System.out.println("enter num: ");
+                int arrPositionUpdate = Integer.parseInt(input.nextLine());
                 for (int i = 0; i < arr.length; i++) {
                     arr[4] = arrPositionUpdate;
                 }
                 String stringArr = Arrays.toString(arr);
 
                 System.out.println(stringArr);
-                System.out.println(arrPositionUpdate);
-
-                if(arrPositionUpdate == 0); break;
+                //System.out.println(arrPositionUpdate);
+                if(arrPositionUpdate != 0) {
+                    System.out.println("enter num again");
+                }
 
 
             }catch (Exception ex){
@@ -30,11 +30,10 @@ public class Main {
             }finally{
                 input.close();
             }
-        }while (arrPositionUpdate != 0);
+        }
 
 
 
 
 
     }
-}
